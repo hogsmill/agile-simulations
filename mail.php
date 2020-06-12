5f71e04e9a18d6ba36c1e21c738d209ad881d1b0
hogsmill <info@hogsmill.com>
2020-06-12 17:22:32 +0100

    contact

--- mail.php
+++ mail.php
@@ -2,10 +2,14 @@
 
 // Temp Solution: Write messages to a file
 //
+$data = urldecode($_GET["data"])
 
-$action = $_GET["action"];
-$email = $_GET["email"];
 $date = date("D M j G:i:s T Y");
+$action = $data["action"];
+$email = $data["email"];
+if ($action == "contact") {
+  $comments = $data["conmments"];
+}
 
 $f = fopen("mail.txt", "a") or die("Unable to open file!");
 
@@ -13,6 +17,9 @@ fwrite($f, "-------------------------------\n");
 fwrite($f, "date: $date\n");
 fwrite($f, "action: $action\n");
 fwrite($f, "email:  $email\n");
+if ($action == "contact") {
+  fwrite($f, "commecnts:  $email\n");
+}
 
 //$to      = "info@hogsmill.com";
 //$subject = "the subject";
