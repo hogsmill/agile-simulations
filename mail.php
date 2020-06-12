c59356a5dcff1d045a8800263d7634f59b11dac8
hogsmill <info@hogsmill.com>
2020-06-12 16:43:45 +0100

    subscribe

---
+++ mail.php
@@ -0,0 +1,25 @@
+<?php
+
+// Temp Solution: Write messages to a file
+//
+
+$action = $_GET["action"];
+$email = $_GET["email"];
+
+$f = fopen("mail.txt", "a") or die("Unable to open file!");
+
+fwrite($f, "-------------------------------\n");
+fwrite($f, "action: $action\n");
+fwrite($f, "email:  $email\n");
+
+//$to      = "info@hogsmill.com";
+//$subject = "the subject";
+//$message = "hello";
+//$headers = array(
+//    "From" => "info@agilesimulations.co.uk",
+//    "Reply-To" => "info@agilesimulations.co.uk",
+//    "X-Mailer" => "PHP/" . phpversion()
+//);
+//
+//mail($to, $subject, $message, $headers);
+?>
