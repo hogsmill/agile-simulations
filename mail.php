73c337c0feab06d725a2e473385b5a4bc86b885b
hogsmill <info@hogsmill.com>
2020-08-26 21:37:59 +0100

    mail bug

--- mail.php
+++ mail.php
@@ -4,11 +4,11 @@ $date = date("D M j G:i:s T Y");
 $action = $_GET["action"];
 $email = urldecode($_GET["email"]);
 $comments = "SUBSCRIBE";
-if ($action == "contact" || $action = "suggest") {
+if ($action == "contact" || $action == "suggest") {
   $comments = urldecode($_GET["comments"]);
 }
 
-$message = "date: $date\naction: $action\nemail: $email\ncomments: $comments\n";
+$message = "----------------------------\ndate: $date\naction: $action\nemail: $email\ncomments: $comments\n";
 
 $f = fopen("mail.txt", "a") or die("Unable to open file!");
 
