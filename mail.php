3fe43dbdb08976267cdda745faff7ebce6963acf
hogsmill <info@hogsmill.com>
2021-06-03 15:24:31 +0100

    mail.php

--- mail.php
+++ mail.php
@@ -2,10 +2,14 @@
 
 $date = date("D M j G:i:s T Y");
 $action = $_GET["action"];
+$name = urldecode($_GET["name"]);
 $email = urldecode($_GET["email"]);
+$mobile = urldecode($_GET["mobile"]);
+$company = urldecode($_GET["company"]);
+$game = urldecode($_GET["game"]);
 $comments = urldecode($_GET["comments"]);
 
-$message = "----------------------------\ndate: $date\naction: $action\nemail: $email\ncomments: $comments\n";
+$message = "----------------------------\ndate: $date\naction: $action\nname: $name\nemail: $email\nmobile: $mobile\ncompany: $company\ngame\n: $game\ncomments: $comments\n";
 
 $f = fopen("mail.txt", "a") or die("Unable to open file!");
 
